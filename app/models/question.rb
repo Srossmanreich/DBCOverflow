3419a2d9a53b4856bccdcfc8f47b14484231493f
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-04-21 16:21:43 -0700

    Fix table bugs

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,12 @@
 class Question < ActiveRecord::Base
 
+  has_many :votes
   has_many :comments, as: :interactive
   belongs_to :user
+
+  def points
+    votes.sum(:value)
+  end
 
   validates :title, :content, presence: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 20.times do
   question = Question.create!( title: Faker::Company.catch_phrase,
-               content: Faker::Company.catch_phrase)
+      content: Faker::Company.catch_phrase)
 
   vote_count = rand(100)
   vote_count.times do
